9fe1339132caf63984c9c2d749f9fd296e101d51
Philippe St-Laurent-Recoura <71201023+Philou213@users.noreply.github.com>
2024-10-11 20:33:52 -0400

    change repo

--- Scripts/Buttons/PlayButton.gd
+++ Scripts/Buttons/PlayButton.gd
@@ -1,7 +1,4 @@
 extends Button
 
-func _ready() -> void:
-	SubsystemManager.get_music_manager().play_music("res://Assets/Music1.mp3")
-
 func _on_pressed() -> void:
-	SubsystemManager.get_level_manager().load_level("res://game.tscn")
+	SubsystemManager.get_level_manager().load_level("res://Scenes/game.tscn")
