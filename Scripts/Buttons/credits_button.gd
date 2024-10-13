716a3e9b9617a1541c4e443a5d471a723f1695d3
Psonbre <adam.maps.2003@gmail.com>
2024-10-13 09:21:48 -0400

    main menu updated

--- Scripts/Buttons/credits_button.gd
+++ Scripts/Buttons/credits_button.gd
@@ -4,3 +4,10 @@ func _on_pressed() -> void:
 	SubsystemManager.get_sound_manager().play_sound("res://Assets/Sounds/button_click.ogg", 0, 1)
 	get_tree().root.get_node("Game").load_credits()
 	focus_mode = FocusMode.FOCUS_NONE
+
+func _on_mouse_entered():
+	scale = 1.1 * Vector2.ONE
+
+
+func _on_mouse_exited():
+	scale = 1.0 * Vector2.ONE
--- Scripts/level_name.gd
+++ Scripts/level_name.gd
@@ -1,10 +1,17 @@
-extends Label
+extends Control
+
+@export var vertical_speed = 0.5
+@export var vertical_intensity = 20
+@export var horizontal_speed = 1
+@export var horizontal_intensity = 15
 
 var start_position
+var rand
 
 func _ready():
 	start_position = position
+	rand = randf() * 150
 
 func _process(delta):
-	position = start_position + Vector2(cos(Time.get_unix_time_from_system()) * 15, sin(Time.get_unix_time_from_system() / 2) * 20)
+	position = start_position + Vector2(cos(Time.get_unix_time_from_system() * horizontal_speed + rand) * horizontal_intensity, sin(Time.get_unix_time_from_system() + rand * vertical_speed) * vertical_intensity)
 	pass
