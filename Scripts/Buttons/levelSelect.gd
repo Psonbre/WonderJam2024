b976603b5aec57707b7f596ad60c3bd602e0741f
Psonbre <adam.maps.2003@gmail.com>
2024-10-12 22:09:38 -0400

    Merge branch 'main' of https://github.com/Psonbre/WonderJam2024

--- Scripts/Buttons/levelSelect.gd
+++ Scripts/Buttons/levelSelect.gd
@@ -3,4 +3,5 @@ extends Button
 @export var levelName : String
 
 func _on_pressed() -> void:
+	SubsystemManager.get_sound_manager().play_sound("res://Assets/Sounds/button_click.ogg", 0, 1)
 	SubsystemManager.get_level_manager().load_level(levelName)
--- Scripts/door.gd
+++ Scripts/door.gd
@@ -3,4 +3,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player :
+		SubsystemManager.get_sound_manager().play_sound("res://Assets/Sounds/winJingle.ogg")
 		body.win(self)
