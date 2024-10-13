99a63cfb796d624f81f355515548c5bf47008806
Psonbre <adam.maps.2003@gmail.com>
2024-10-12 23:38:10 -0400

    merge fix

--- Scripts/level_name.gd
+++ Scripts/level_name.gd
@@ -3,8 +3,8 @@ extends Label
 var start_position
 
 func _ready():
-	start_position = global_position
+	start_position = position
 
 func _process(delta):
-	global_position = start_position + Vector2(cos(Time.get_unix_time_from_system()/2) * 15, sin(Time.get_unix_time_from_system() / 4) * 20)
+	position = start_position + Vector2(cos(Time.get_unix_time_from_system()) * 15, sin(Time.get_unix_time_from_system() / 2) * 20)
 	pass
